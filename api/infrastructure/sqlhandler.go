2c6b247cf33c97110141b4509305de8eda20c6ac
畠 翔紀 <hata-shoki@a-tm.co.jp>
2020-10-31 15:51:13 +0900

    update: #2 infrastructure/SqlHanderを、SqlHandlerのインターフェースに準拠するよう変更

--- api/infrastructure/sqlhandler.go
+++ api/infrastructure/sqlhandler.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 
 	_ "github.com/go-sql-driver/mysql"
+	"api/interfaces/database"
 )
 
 // SqlHandler はSQLハンドラを扱う型です。
@@ -11,8 +12,8 @@ type SqlHandler struct {
 	Conn *sql.DB
 }
 
-// NewSqlHandler は SqlHandlerを生成します。
-func NewSqlHandler() *SqlHandler {
+// NewSqlHandler は、生成したSqlHandlerのインターフェースを返します。
+func NewSqlHandler() database.SqlHandler {
 	conn, err := sql.Open("mysql", "root:@tcp(db:3306)/pomodoro")
 	if err != nil {
 		panic(err.Error)
@@ -20,4 +21,52 @@ func NewSqlHandler() *SqlHandler {
 	sqlHandler := new(SqlHandler)
 	sqlHandler.Conn = conn
 	return sqlHandler
+}
+
+func (handler *SqlHandler) Execute(statement string, args...interface{}) (database.Result.error) {
+	res := SqlResult{}
+	result, err := handler.Conn.Exec(statement, args...)
+	if err != nil {
+		return res, err
+	}
+	res.Result = result
+	return res, nil
+}
+
+func (handler *SqlHandler) Query(statement string, args ...interface{}) (database.Row, error) {
+    rows, err := handler.Conn.Query(statement, args...)
+    if err != nil {
+        return new(SqlRow), err
+    }
+    row := new(SqlRow)
+    row.Rows = rows
+    return row, nil
+}
+
+type SqlResult struct {
+	Result sql.Result
+}
+
+func (r SqlResult) LastInsertId() (int64, error) {
+    return r.Result.LastInsertId()
+}
+
+func (r SqlResult) RowsAffected() (int64, error) {
+    return r.Result.RowsAffected()
+}
+
+type SqlRow struct {
+    Rows *sql.Rows
+}
+
+func (r SqlRow) Scan(dest ...interface{}) error {
+    return r.Rows.Scan(dest...)
+}
+
+func (r SqlRow) Next() bool {
+    return r.Rows.Next()
+}
+
+func (r SqlRow) Close() error {
+    return r.Rows.Close()
 }
