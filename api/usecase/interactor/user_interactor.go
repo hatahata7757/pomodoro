5b9488781c2e7fbe01dd384253180109dcb84337
畠 翔紀 <hata-shoki@a-tm.co.jp>
2020-10-31 16:58:04 +0900

    add: #2 ユーザー一覧取得のUse Caseの作成

--- api/usecase/interactor/user_interactor.go
+++ api/usecase/interactor/user_interactor.go
@@ -12,3 +12,9 @@ func (interactor *UserInteractor) Add(u model.User) (err error) {
 	_, err := interactor.UserRepository.Store(u)
 	return
 }
+
+// Users は UserRepository を通して FindAll を実行するためのメソッドです。
+func (interactor *UserInteractor) Users() (user model.Users, err error) {
+	user, err = interactor.UserRepository.FindAll()
+	return
+}
