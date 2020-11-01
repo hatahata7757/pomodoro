ae53ed8da00d360fa0d3fbdd6e4334e8b1b23a63
Shoki Hata <37888628+hatahata7757@users.noreply.github.com>
2020-11-01 13:03:17 +0900

    Revert "#2ユーザー登録機能の作成"

--- api/interfaces/controllers/context.go
+++
@@ -1,9 +0,0 @@
-package controllers
-
-// Context は gin.Contextで利用するメソッドのインターフェースです。
-type Context interface {
-	Param(string) string
-	Bind(interface{}) error
-	Status(int)
-	JSON(int, interface{})
-}
