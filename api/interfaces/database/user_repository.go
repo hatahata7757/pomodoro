41e170ca27a7f28eba1a11e2e39511fb6e568b6b
Shoki Hata <37888628+hatahata7757@users.noreply.github.com>
2020-11-01 13:11:04 +0900

    Revert "Revert "#2ユーザー登録機能の作成""

---
+++ api/interfaces/database/user_repository.go
@@ -0,0 +1,64 @@
+package database
+
+import "api/domain/model"
+
+// SqlHandler を埋め込んだ UserRepository 型を定義
+type UserRepository struct {
+	SqlHandler
+}
+
+// Store は User を保存するメソッドです
+func (repo *UserRepository) Store(u model.User) (id int, err error) {
+	result, err := repo.Execute(
+		"INSERT INTO users (name) VALUES (?)", u.Name,
+	)
+	if err != nil {
+		return
+	}
+	id64, err := result.LastInsertId()
+	if err != nil {
+		return
+	}
+	id = int(id64)
+	return
+}
+
+// FindById は 引数で渡された id に一致する User を取得するメソッドです
+func (repo *UserRepository) FindById(identifier int) (user model.User, err error) {
+	row, err := repo.Query("SELECT id, name, FROM users WHERE id = ?", identifier)
+	defer row.Close()
+	if err != nil {
+		return
+	}
+	var id int
+	var name string
+	row.Next()
+	if err = row.Scan(&id, &name); err != nil {
+		return
+	}
+	user.ID = id
+	user.Name = name
+	return
+}
+
+// FindAll は全ての User を取得するメソッドです
+func (repo *UserRepository) FindAll() (users model.Users, err error) {
+	rows, err := repo.Query("SELECT id, name FROM users")
+	defer rows.Close()
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		var id int
+		var name string
+		if err := rows.Scan(&id, &name); err != nil {
+			continue
+		}
+		user := model.User{
+			ID:   id,
+			Name: name,
+		}
+		users = append(users, user)
+	}
+	return
+}
