2f77ade9c9ca1d75de040f165201c8f9f6528d45
hatahata7757 <syouki100241@gmail.com>
2021-01-09 19:10:27 +0900

    add: #8 DBテーブルをセットアップ時にtasksテーブルをcreateするように処理を追加

--- docker/mysql/sql/001-create-tables.sql
+++ docker/mysql/sql/001-create-tables.sql
@@ -1,12 +1,26 @@
 ---- drop ----
 DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `tasks`:
 
 ---- create ----
-create table IF not exists `users`
+create table IF NOT EXISTS `users`
 (
   `id`               INT(20) AUTO_INCREMENT,
   `name`             VARCHAR(20) NOT NULL,
   `created_at`       Datetime DEFAULT NULL,
   `updated_at`       Datetime DEFAULT NULL,
     PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+create table IF NOT EXISTS `tasks`
+(
+  `id`               INT(20) AUTO_INCREMENT,
+  `name`             VARCHAR(20) NOT NULL,
+  `done`             BOOLEAN DEFAULT false,
+  `user_id`          INT(20),
+  `created_at`       Datetime DEFAULT NULL,
+  `updated_at`       Datetime DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY(`user_id`),
+    REFERENCES `users`(`users_id`)
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
